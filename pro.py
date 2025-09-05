c53593536648561ef942054859cf7299a575b168
m0zs0 <monoki.zsolt@moriczref.hu>
2025-09-05 12:15:53 +0200

    done

--- pro.py
+++ pro.py
@@ -1,1 +1,34 @@
-print("Szia")
+print("Szia", end=" ")
+print("Tibi")
+print("Szia", "Tóth", "Karcsi", "!", sep="")
+print("Szia Tóth Karcsi!")
+nev = "Tóth Karcsi"
+print(f"Szia {nev}!")
+print("Szia", "Peti", sep="\n", end="!") #\t
+#adattípusok
+# int()
+# float()
+# str()
+# bool()
+# list()
+# set()
+# tuple()
+print()
+print(int("5"))
+print(float(5))
+print(type(str(5.0)))
+print(bool(-1))
+print(["hétfő", "kedd", "szerda"])
+napok = ["hétfő", "kedd", "szerda"]
+print(f"Napok: {napok}")
+nevek = ["Tibi", "Sanyi", "Tibi"]
+print(f"Nevek: {set(nevek)}")
+print(tuple([1,2,3]))
+
+
+# HF: git parancsok:
+# git init : adott mappa verziókövetésének indítása
+# git add . :
+
+
+
