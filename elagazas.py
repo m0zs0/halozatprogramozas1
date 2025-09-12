1ef353052d65ced13796df71add2f9bc1f091819
m0zs0 <monoki.zsolt@moriczref.hu>
2025-09-12 10:32:06 +0200

    tesztelés

--- elagazas.py
+++ elagazas.py
@@ -13,4 +13,32 @@ else:
     print("Kisebbre gondoltam") 
 
 
+# készíts függvényt előjel néven, mely átvesz egy egész számot és az előjelét adja vissza
 
+def elojel(szam):
+    if szam >= 0:
+        return "+"
+    else:
+        return "-" 
+
+szam = int(input("Kérek egy számot előjellel: "))
+
+print(f"A {szam} elojele: {elojel(szam)}.")
+
+print(f"A {szam} elojele: {'+' if szam>=0 else '-'}.")
+
+def tesztesetek():
+    teszt(5,"+")
+    teszt(-3,"-")
+    teszt(-1,"-")
+    teszt(0,"+")
+    teszt(1,"+")
+    #...
+
+def teszt(szam, elvart_elojel):
+    if elojel(szam) == elvart_elojel:
+        print(f"elojel({szam})=={elvart_elojel} true")
+    else:
+        print(f"elojel({szam})=={elvart_elojel} false")
+
+tesztesetek()
