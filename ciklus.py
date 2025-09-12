3eee0ad8d70ffd82e0815094510ee14fce8529f0
m0zs0 <monoki.zsolt@moriczref.hu>
2025-09-12 11:55:21 +0200

    paros darab

--- ciklus.py
+++ ciklus.py
@@ -1,1 +1,21 @@
 # ciklusok
+
+lista = [1, 2, 3, 4, 5]
+
+def osszegez(lista):
+    osszeg = 0
+    for elem in lista:
+        osszeg += elem
+    return osszeg
+
+def paros_szamok_darabszama(lista):
+    db = 0
+    for szam in lista:
+        if szam%2 == 0:
+            db+=1
+    return db
+
+
+print(f"A lista elemeinek összege: {osszegez(lista)}")
+
+print(f"A lista páros elemeinek darabszáma: {paros_szamok_darabszama(lista)}")
