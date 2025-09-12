0e6726d8da36046d75dce09cf313dd7369e4056f
m0zs0 <monoki.zsolt@moriczref.hu>
2025-09-12 10:03:28 +0200

    gondolt szam

---
+++ elagazas.py
@@ -0,0 +1,16 @@
+import random
+
+gondolt_szam = random.randint(1,3)
+
+#print(f"A gondolt szám: {gondolt_szam}")
+bekert_szam = int(input("Kérem a tippet: "))
+
+if gondolt_szam == bekert_szam:
+    print("Eltaláltad!")
+elif gondolt_szam > bekert_szam:
+    print("Nagyobbra gondoltam!")
+else:
+    print("Kisebbre gondoltam") 
+
+
+
